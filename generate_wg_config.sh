0a8ab5158a523167a2cb36f2e5a9d97345afc999
Thom Wiggers <thom@thomwiggers.nl>
2019-11-11 17:44:11 +0100

    Allow setting up unmanaged clients

--- generate_wg_config.sh
+++ generate_wg_config.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+
 if [ "$2" = "" ]; then
     echo "Usage: $0 <host> <ip-postfix>"
     exit 1
+fi
+
+unmanaged=/bin/false
+if [ "$3" = "unmanaged" ]; then
+    echo "Setting up unmanaged client"
+    unmanaged=/bin/true
 fi
 
 host="$1"
@@ -14,7 +22,7 @@ if grep -B4 "$client_ipv4" host_vars/*/vars group_vars/vpn/vars; then
     exit 1
 fi
 
-if ! grep "$host" hosts > /dev/null; then
+if ! $unmanaged && ! grep "$host" hosts > /dev/null; then
     echo "Host not found"
     exit 1
 fi
@@ -31,46 +39,60 @@ psk="$(wg genpsk)"
 allowed_ips="10.20.0.0/24, 2a01:4f8:231:3ac7:1337:1000::/96"
 
 # Disabled until we re-add adding clients that aren't setup with ansible
-#qrencode -t ansiutf8 <<EOF
-#[Interface]
-#Address = $client_ipv4
-#PrivateKey = $private_key
-#
-#[Peer]
-#PublicKey = $server_public
-#PresharedKey = $psk
-#AllowedIPs = $allowed_ips
-#Endpoint = archeron.rded.nl:51820
-#PersistentKeepalive = 2500
-#EOF
+if $unmanaged
+then
+	cat <<-EOF > client-config.ini
+	[Interface]
+	Address = $client_ipv4
+	PrivateKey = $private_key
 
-tee clients.txt <<EOF
-public_key: "$public_key"
-preshared_key: "$psk"
-EOF
+	[Peer]
+	PublicKey = $server_public
+	PresharedKey = $psk
+	AllowedIPs = $allowed_ips
+	Endpoint = archeron.rded.nl:51820
+	PersistentKeepalive = 2500
+	EOF
 
-cat <<EOF >> host_vars/$host/vars
+	tee clients.txt <<-EOF
+	public_key: "$public_key"
+	preshared_key: "$psk"
+	EOF
 
-# Wireguard
-wireguard_public_key: "$public_key"
-wireguard_private_key: "{{ ${host}_wireguard_private_key }}"
-wireguard_preshared_key: "{{ ${host}_wireguard_preshared_key }}"
-wireguard_client_ip: "$client_ipv4"
-wireguard_client_ipv6: "$client_ipv6"
-EOF
+	qrencode -t ansiutf8 < client-config.ini
 
-if [ -f "host_vars/$host/vault" ]; then
-    vault="$(ansible-vault view host_vars/$host/vault)"
+	cat <<-EOF >> group_vars/vpn/vars
+	# New VPN client
+	  - name: $host
+	    public_key: $server_public
+	    preshared_key: $psk
+	    client_ip: $client_ipv4
+	    client_ipv6: $client_ipv6
+	EOF
 else
-    vault="---"
+	cat <<-EOF >> host_vars/$host/vars
+	# Wireguard
+	wireguard_public_key: "$public_key"
+	wireguard_private_key: "{{ ${host}_wireguard_private_key }}"
+	wireguard_preshared_key: "{{ ${host}_wireguard_preshared_key }}"
+	wireguard_client_ip: "$client_ipv4"
+	wireguard_client_ipv6: "$client_ipv6"
+	EOF
+
+    if [ -f "host_vars/$host/vault" ]
+    then
+        vault="$(ansible-vault view host_vars/$host/vault)"
+    else
+        vault="---"
+    fi
+	vault=$(cat <<-EOF
+	$vault
+
+	# Wireguard
+	${host}_wireguard_private_key: "$private_key"
+	${host}_wireguard_preshared_key: "$preshared_key"
+	EOF
+    )
+    echo "$vault" | ansible-vault encrypt --output "host_vars/$host/vault" -
 fi
-vault=$(cat <<EOF
-$vault
-
-# Wireguard
-${host}_wireguard_private_key: "$private_key"
-${host}_wireguard_preshared_key: "$preshared_key"
-EOF
-)
-
-echo "$vault" | ansible-vault encrypt --output "host_vars/$host/vault" -
+# vim: set ft=sh ts=4 sw=4 tw=0 noet :
